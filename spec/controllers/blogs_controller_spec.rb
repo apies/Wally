3f5c871c454e26b4814d07de88917e5e7b033e9a
Alan Pies <alan.lee.pies@gmail.com>
2013-01-09 18:36:40 -0800

    writing controller tests with last commits mentioned strategy until i stub everything out

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -4,29 +4,36 @@ describe BlogsController do
 
   
 
-  # describe "GET 'index'" do
+  describe "#index" do
 
-  #   before :each do
-  #     stub_request(:get, "https://www.googleapis.com/discovery/v1/apis/blogger/v3/rest").
-  #         with(:headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'google-api-ruby-client/0.5.0 Mac OS X/10.8.2'}).
-  #         to_return(:status => 200, :body => "", :headers => {})
-  #   end
+    before :all do
+      Googler.extend GooglerTestingService
+      Googler.create_test_client
+      #stub_request(:get, "https://www.googleapis.com/discovery/v1/apis/blogger/v3/rest").
+       #   with(:headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'google-api-ruby-client/0.5.0 Mac OS X/10.8.2'}).
+        #  to_return(:status => 200, :body => "", :headers => {})
+    end
 
 
-  #   it "returns http success" do
-  #     # stub_request(:get, "https://www.googleapis.com/discovery/v1/apis/blogger/v3/rest").
-  #     #    with(:headers => {'Accept'=>'*/*', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'google-api-ruby-client/0.5.0 Mac OS X/10.8.2'}).
-  #     #    to_return(:status => 200, :body => "", :headers => {})
-  #     get 'index'
-  #     response.should be_success
-  #   end
-  # end
+    it "returns http success" do
+      xhr :get, :index, {}
+      response.should be_success
+    end
 
-  # describe "GET 'show'" do
-  #   it "returns http success" do
-  #     get 'show'
-  #     response.should be_success
-  #   end
-  # end
+    it "returns a list of my blogs" do
+      xhr :get, :index, {}
+    end
+ end
+
+  describe "#show" do
+
+    let(:llb_id) { '2510490903247292153'}
+
+    it "returns http success" do
+      xhr :get, :show, :id => llb_id
+      JSON.parse(response.body)['name'].should eq "Loud Like Bulls"
+      response.should be_success
+    end
+  end
 
 end
